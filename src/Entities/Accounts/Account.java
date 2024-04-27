a8aa992b88ff8d83145df6bd75b363f6af605355
rolemberg <vinnie.nsts@gmail.com>
2024-04-27 01:55:23 -0300

    first commit

---
+++ src/Entities/Accounts/Account.java
@@ -0,0 +1,29 @@
+package Entities.Accounts;
+
+public class Account {
+
+    private int accountNumber;
+    private Double amount;
+
+    public Account(int accountNumber, Double amount) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
+
---
+++ src/Entities/Agencies/Agency.java
@@ -0,0 +1,19 @@
+package Entities.Agencies;
+
+public class Agency {
+
+    private String number;
+
+    public Agency(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
+
